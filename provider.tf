2ba32d1ac591393924eb4e5d848a0c9fda9f89d9
MAHENDRA4593 <madan452218@gmail.com>
2025-10-23 15:40:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
   aws = {
     source  = "hashicorp/aws"
     version = "6.17.0"
+ region = "us-east-1"
   }
+}
+assume_role {
+    role_arn     = "arn:aws:iam::225989358737:role/terraformproj"
+    session_name = "terraform-session"
 }
 }
