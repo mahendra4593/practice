a47408c4cd05965981958d3288ce8b8d69f219fe
MAHENDRA4593 <madan452218@gmail.com>
2025-10-23 15:24:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,9 @@
-required_providers {
+# terraform block providers version,backend,locking.
+terraform {
+ required_providers {
   aws = {
     source  = "hashicorp/aws"
     version = "6.17.0"
   }
+}
 }
